0d1a0fe2e477c78ff6a6b73aeea668c139cb249c
Kinjal Das <kinjal.das86@gmail.com>
2019-10-26 19:38:01 +0530

    1st stable version

--- React/my-first-react-app/src/App.js
+++ React/my-first-react-app/src/App.js
@@ -14,7 +14,9 @@ class App extends Component {
       loggedin: false,
       showlogin: true,
       username: null,
-      password: null
+      password: null,
+      addissue: null,
+      showaddissue:false
   }
 
   componentDidMount() {
@@ -34,6 +36,48 @@ class App extends Component {
 
   handleClose = () => {
     this.setState({show:false})
+  }
+
+  showaddissue = () => {
+    this.setState({showaddissue:true})
+  }
+
+  closeaddissue = () => {
+    this.setState({showaddissue:false,addissue:null})
+  }
+
+  async deleteissue(issue){
+    console.log("deleting:" + JSON.stringify(issue));
+    await fetch('http://127.0.0.1:8000/api/issues/'+issue.id,{method:"DELETE"})
+    .then(res => console.log(res))
+    .catch(console.log);
+    window.location.reload();
+    this.setState({show:false})
+  }
+
+  async addissue(formobject){
+    var obj = {};
+    obj.CRID = formobject.CRID.value;
+    obj.Project = formobject.Project.value;
+    obj.Title = formobject.Title.value;
+    obj.Component = formobject.Component.value;
+    obj.Description = formobject.Description.value;
+    obj.Prog_or_Comm = formobject.Prog_or_Comm.value;
+    obj.Open = formobject.Open.checked;
+    console.log("adding:" + JSON.stringify(obj));
+    await fetch('http://127.0.0.1:8000/api/issues/',
+    {
+      method:"POST",
+      headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+      },
+      body:JSON.stringify(obj)
+    })
+    .then(res => console.log(res))
+    .catch(console.log);
+    //this.setState({showaddissue:false,addissue:obj});
+    //window.location.reload();
   }
 
   closelogin = () => {
@@ -70,6 +114,7 @@ class App extends Component {
   render(){
     return (
       <div className="App">
+      {/* Login */}
       <Modal show={this.state.showlogin} onHide={this.closelogin}>
         <Modal.Header closeButton>
           <Modal.Title>Login</Modal.Title>
@@ -98,12 +143,61 @@ class App extends Component {
           </Button>
         </Modal.Footer>
       </Modal>
+      {/* Add Issue */}
+      <Modal size="lg" show={this.state.showaddissue} onHide={this.closeaddissue}>
+        <Modal.Header closeButton>
+          <Modal.Title>Add Issue</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+        <Form>
+          <Form.Text className="text-muted">
+            Enter your Issue Details.
+          </Form.Text>
+          <br/>
+          <Form.Group controlId="formCRID">
+            <Form.Label>CR ID</Form.Label>
+            <Form.Control type="text" placeholder="Enter ALM CR ID" ref={(CRID) => {this.CRID = CRID}}/>
+          </Form.Group>
+          <Form.Group controlId="formProject">
+            <Form.Label>Project</Form.Label>
+            <Form.Control type="text" placeholder="Enter Project (Stanley/Yeti/UPD/Others etc.)" ref={(Project) => {this.Project = Project}}/>
+          </Form.Group>
+          <Form.Group controlId="formTitle">
+            <Form.Label>Title</Form.Label>
+            <Form.Control type="text" placeholder="Enter Issue Title (short description)" ref={(Title) => {this.Title = Title}}/>
+          </Form.Group>
+          <Form.Group controlId="formComponent">
+            <Form.Label>Component</Form.Label>
+            <Form.Control type="text" placeholder="Enter Component (UI/MS/IO/Render etc.)" ref={(Component) => {this.Component = Component}}/>
+          </Form.Group>
+          <Form.Group controlId="formDescription">
+            <Form.Label>Description</Form.Label>
+            <Form.Control as="textarea" rows="5" placeholder="Enter Summary (detailed description)" ref={(Description) => {this.Description = Description}}/>
+          </Form.Group>
+          <Form.Group controlId="formProgOrComm">
+            <Form.Label>Progress/Comments</Form.Label>
+            <Form.Control as="textarea" rows="5" placeholder="Enter Latest Progress/Comments" ref={(Prog_or_Comm) => {this.Prog_or_Comm = Prog_or_Comm}}/>
+          </Form.Group>
+          <Form.Group controlId="formOpen">
+          <Form.Check type="checkbox" label="Open" ref={(Open) => {this.Open = Open}}/>
+          </Form.Group>
+        </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={this.closeaddissue}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={(e) => this.addissue(this)}>
+            Add
+          </Button>
+        </Modal.Footer>
+      </Modal>
         <Navbar bg="dark" variant="dark" expang="lg">
           <Navbar.Brand href="#home">Issue Tracker</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="mr-auto">
-              <Nav.Link><Button>Add Issue</Button></Nav.Link>
+              <Nav.Link><Button onClick={this.showaddissue}>Add Issue</Button></Nav.Link>
               <Nav.Link>{this.state.loggedin?<Button onClick={this.showlogin}>Logout</Button>:<Button onClick={this.showlogin}>Login</Button>}</Nav.Link>
             </Nav>
             <Form inline>
@@ -161,7 +255,7 @@ class App extends Component {
             <Button variant="success" onClick={this.handleClose}>
               Edit
             </Button>
-            <Button variant="danger" onClick={this.handleClose}>
+            <Button variant="danger" onClick={(e) => this.deleteissue(this.state.issue?this.state.issue:null)}>
               Delete
             </Button>
           </Modal.Footer>
