f98f5441ed06e464398162fc6480fdaf42a91d5a
KinjalDas <kinjal.das86@gmail.com>
2019-10-29 19:03:48 +0530

    correcting batch script

--- run.bat
+++ run.bat
@@ -1,1 +1,1 @@
-"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --user-data-dir="C://Chrome dev session" --disable-web-security
+"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --user-data-dir="C://Chrome dev session" --disable-web-security --user-data-dir="C://Temp"
